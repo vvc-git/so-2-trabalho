215693797b5ba73da35ca276166524d96169598b
victor.valle <victorvcunha@hotmail.com>
2023-08-09 15:52:06 -0300

    base

---
+++ include/machine/riscv/riscv_ic.h
@@ -0,0 +1,158 @@
+// EPOS RISC-V IC Mediator Declarations
+
+#ifndef __riscv_ic_h
+#define __riscv_ic_h
+
+#include <architecture/cpu.h>
+#include <machine/ic.h>
+#include <system/memory_map.h>
+
+__BEGIN_SYS
+
+// Core Local Interrupter (CLINT)
+class CLINT
+{
+private:
+    typedef CPU::Reg Reg;
+    typedef CPU::Reg32 Reg32;
+    typedef CPU::Reg64 Reg64;
+    typedef CPU::Phy_Addr Phy_Addr;
+    typedef CPU::Log_Addr Log_Addr;
+
+public:
+    static const unsigned int IRQS = 16;
+
+    // Interrupts ([M|S]CAUSE with interrupt = 1)
+    enum : unsigned long {
+        IRQ_USR_SOFT            = 0,
+        IRQ_SUP_SOFT            = 1,
+        IRQ_MAC_SOFT            = 3,
+        IRQ_USR_TIMER           = 4,
+        IRQ_SUP_TIMER           = 5,
+        IRQ_MAC_TIMER           = 7,
+        IRQ_USR_EXT             = 8,
+        IRQ_SUP_EXT             = 9,
+        IRQ_MAC_EXT             = 11,
+        INTERRUPT               = 1UL << (Traits<CPU>::WORD_SIZE - 1),
+        INT_MASK                = ~INTERRUPT
+
+    };
+
+    // Registers offsets from CLINT_BASE
+    enum {                                // Description
+        MSIP                    = 0x0000, // Per-HART 32-bit software interrupts (IPI) trigger; each HART is offset by 4 bytes from MSIP
+        MTIMECMP                = 0x4000, // Per-HART 64-bit timer compare register; lower 32 bits stored first; each HART is offset by 8 bytes from MTIMECMP
+        MTIME                   = 0xbff8, // Timer counter shared by all HARTs lower 32 bits stored first
+    };
+
+    // MTVEC modes
+    enum Mode {
+        DIRECT  = 0,
+        INDEXED = 1
+    };
+
+public:
+    static void mtvec(Mode mode, Phy_Addr base) {
+    	Reg tmp = (base & -4UL) | (Reg(mode) & 0x3);
+        ASM("csrw mtvec, %0" : : "r"(tmp) : "cc");
+    }
+
+    static Reg mtvec() {
+        Reg value;
+        ASM("csrr %0, mtvec" : "=r"(value) : : );
+        return value;
+    }
+
+    static Reg64 mtime() { return *reinterpret_cast<Reg64 *>(Memory_Map::CLINT_BASE + MTIME); }
+    static void  mtimecmp(Reg64 v) { *reinterpret_cast<Reg64 *>(Memory_Map::CLINT_BASE + MTIMECMP) = v; }
+};
+
+class IC: private IC_Common, private CLINT
+{
+    friend class Setup;
+    friend class Machine;
+
+private:
+    typedef CPU::Reg Reg;
+
+public:
+    static const unsigned int EXCS = CPU::EXCEPTIONS;
+    static const unsigned int IRQS = CLINT::IRQS;
+    static const unsigned int INTS = EXCS + IRQS;
+
+    using IC_Common::Interrupt_Id;
+    using IC_Common::Interrupt_Handler;
+
+    enum {
+        INT_SYS_TIMER = EXCS + IRQ_MAC_TIMER
+    };
+
+public:
+    IC() {}
+
+    static Interrupt_Handler int_vector(Interrupt_Id i) {
+        assert(i < INTS);
+        return _int_vector[i];
+    }
+
+    static void int_vector(Interrupt_Id i, const Interrupt_Handler & h) {
+        db<IC>(TRC) << "IC::int_vector(int=" << i << ",h=" << reinterpret_cast<void *>(h) <<")" << endl;
+        assert(i < INTS);
+        _int_vector[i] = h;
+    }
+
+    static void enable() {
+        db<IC>(TRC) << "IC::enable()" << endl;
+        CPU::mie(CPU::MSI | CPU::MTI | CPU::MEI);
+    }
+
+    static void enable(Interrupt_Id i) {
+        db<IC>(TRC) << "IC::enable(int=" << i << ")" << endl;
+        assert(i < INTS);
+        enable();
+        // TODO: this should handle individual INTs and also be done at PLIC
+    }
+
+    static void disable() {
+        db<IC>(TRC) << "IC::disable()" << endl;
+        CPU::miec(CPU::MSI | CPU::MTI | CPU::MEI);
+    }
+
+    static void disable(Interrupt_Id i) {
+        db<IC>(TRC) << "IC::disable(int=" << i << ")" << endl;
+        assert(i < INTS);
+        disable();
+        // TODO: this should handle individual INTs and also be done at PLIC
+    }
+
+    static Interrupt_Id int_id() {
+        // Id is retrieved from [m|s]cause even if mip has the equivalent bit up, because only [m|s]cause can tell if it is an interrupt or an exception
+        Reg id = CPU::mcause();
+        if(id & INTERRUPT)
+            return irq2int(id & INT_MASK);
+        else
+            return (id & INT_MASK);
+    }
+
+    static int irq2int(int i) { return i + EXCS; }
+    static int int2irq(int i) { return i - EXCS; }
+
+private:
+    static void dispatch();
+
+    // Logical handlers
+    static void int_not(Interrupt_Id i);
+    static void exception(Interrupt_Id i);
+
+    // Physical handler
+    static void entry() __attribute((naked, aligned(4)));
+
+    static void init();
+
+private:
+    static Interrupt_Handler _int_vector[INTS];
+};
+
+__END_SYS
+
+#endif
