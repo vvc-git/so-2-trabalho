31f4c25125a068dc1fcf1e5754b50d25c81a44a3
victor.valle <victorvcunha@hotmail.com>
2023-09-15 00:11:28 -0300

    Algumas bits setaddos

--- app/hello/hello.cc
+++ app/hello/hello.cc
@@ -1,6 +1,8 @@
 #include <utility/ostream.h>
 
 #include <machine/riscv/riscv_cadence.h>
+#include <utility/string.h>
+#include <machine/riscv/sifive_u/sifive_u_memory_map.h>
 
 using namespace EPOS;
 
@@ -12,7 +14,11 @@ int main()
     cout << "\n\n ************ TESTANDO CADENCE ************\n\n" << endl;
 
     Cadence net = Cadence();
-    cout << "Ponteiro para o objeto Cadence: " << &net << "\n";
+    int teste;
+    int * addr = reinterpret_cast<int *>(Memory_Map::ETH_BASE + 0x18);
+    memcpy(&teste, addr, sizeof(int));
+    cout << teste << endl;
+    cout << &net << endl;
 
     cout << "\n\n ******************* FIM *******************\n\n" << endl;
 
--- include/machine/riscv/riscv_cadence.h
+++ include/machine/riscv/riscv_cadence.h
@@ -103,7 +103,6 @@ protected:
     typedef CPU::Reg32 Reg32;
     typedef CPU::Log_Addr Log_Addr;
     typedef CPU::Phy_Addr Phy_Addr;
-    typedef Memory_Map::ETH_BASE ETH_BASE;
     // USAR NOSSO BUFFER
     typedef MMU::DMA_Buffer DMA_Buffer;
     // Ver como usar
@@ -115,8 +114,17 @@ protected:
 public:
     // Register offsets
     enum {
-        NETWORK_CONTROL = 0x00000000,
-        RECEIVE_STATUS =  0x00000020,
+        NETWORK_CONTROL = 0x0000000000,
+        NETWORK_CONFIG = 0x0000000004,
+        NETWORK_STATUS =  0x0000000008,
+        TRANSMIT_STATUS = 0x0000000014,
+        RECEIVE_Q_PTR   = 0x0000000018,
+        TRANSMIT_Q_PTR   = 0x000000001C,
+        RECEIVE_STATUS =  0x0000000020,
+        INT_DISABLE    =  0x000000002C, 
+        SPEC_ADD1_BOTTOM = 0x0000000088
+
+        
     };
 
     // Network Control Register bits
@@ -124,42 +132,125 @@ public:
         CLEAR_ALL_STATS_REGS = 1 << 5,
     };
 
+    // Network Config Register bits
+    enum {
+        FULL_DUPLEX = 1 << 2,
+        GIGABIT_MODE_ENABLE = 1 << 10,
+        NO_BROADCAST = 1 << 5,
+        MULTICAST_HASH_ENABLE = 1 << 6,
+        COPY_ALL_FRAMES = 1 << 4,
+        RECEIVE_CHECKSUM_OFFLOAD_ENABLE = 1 << 24,
+        PAUSE_ENABLE = 1 << 13,
+        MDC_CLOCK_DIVISION = 000 << 18,
+
+
+    };
+
     // construtor
     Cadence();
-
-    // destrutor
-    ~Cadence();
 
     // função que seta os valores dos registradores mapeados em memória
     void set_reg(unsigned long int pointer, unsigned int value);
 
     // função que retorna or lógico dos valores passados por parâmetros
-    void set_bits(unsigned long int pointer, unsigned long int pointer);
+    void set_bits(unsigned long int pointer, unsigned int value);
 };
 
 Cadence::Cadence() {
-    // Clear the network control register
+
+    // 1. Clear the network control register
+    //  Write 0x0 to the gem.network_control register.
     Cadence::set_reg(NETWORK_CONTROL, 0x0);
 
-    // Clear the statistics registers
+    // 2. Clear the statistics registers
+    // Write a 1 to the gem.network_control [clear_all_stats_regs].
     Cadence::set_bits(NETWORK_CONTROL, CLEAR_ALL_STATS_REGS);
 
-    //Clear the status registers
-    Cadence::set_reg(RECEIVE_STATUS, 0x0);
+    // 3. Clear the status registers
+
+    // ?? Não conseguimos fazer essa:
+    // Write a 1 to the status registers. 
+    
+    // gem.receive_status = 0x0F
+    Cadence::set_reg(RECEIVE_STATUS, 0x0F);
+
+    // gem.transmit_status = 0xFF.
+    Cadence::set_reg(TRANSMIT_STATUS, 0xFF);
+
+    // 4.Disable all interrupts.
+    // Write 0x7FF_FEFF to the gem. int_disable register.
+    Cadence::set_reg(INT_DISABLE, 0x7FFFEFF);
+
+    // 5. Clear the buffer queues.
+    // Write 0x0 to the gem.receive_q{ , 1}_ptr 
+    // ?? Duvida se esta mesmo sentando, pois testamos com os WR e mesmo tava dando errado
+    Cadence::set_reg(RECEIVE_Q_PTR, 0X0);
+
+    // Write 0x0 to the gem.transmit_q{ , 1}_ptr 
+    // ?? Duvida se esta mesmo sentando, pois testamos com os WR e mesmo tava dando errado
+    Cadence::set_reg(RECEIVE_Q_PTR, 0x0);
+
+
+
+    // Configure the controller
+
+
+    // 1. Enable full duplex. 
+    // a. Write a 1 to the gem.network_config[full_duplex] bit.
+    Cadence::set_bits(NETWORK_CONFIG, FULL_DUPLEX);
+
+    // b. Enable gigabit mode. 
+    // Write a 1 to the gem.network_config[gigabit_mode_enable] bit.
+    Cadence::set_bits(NETWORK_CONFIG, GIGABIT_MODE_ENABLE);
+
+    // c. Enable reception of broadcast or multicast frames. 
+    // Write a 0 to the gem.network_config[no_broadcast] register to enable broadcast frames.
+    Cadence::set_bits(NETWORK_CONFIG, NO_BROADCAST);
+
+    // write a 1 to the gem.network_config[multicast_hash_en] bit to enable multicast frames.
+    Cadence::set_bits(NETWORK_CONFIG, MULTICAST_HASH_ENABLE);
+
+    // d. Enable promiscuous mode.
+    // Write a 1 to the gem.network_config[copy_all_frames] bit.
+    Cadence::set_bits(NETWORK_CONFIG, COPY_ALL_FRAMES);
+
+    // e. Enable TCP/IP checksum offload feature on receive. 
+    // Write a 1 to the gem.network_config[receive_checksum_offload_enable] bit.
+    Cadence::set_bits(NETWORK_CONFIG, RECEIVE_CHECKSUM_OFFLOAD_ENABLE);
+
+    // f.Enable pause frames. Write a 1 to gem.network_config[pause_enable] bit.
+    Cadence::set_bits(NETWORK_CONFIG, PAUSE_ENABLE);
+
+    // g. Set the MDC clock divisor. 
+    // Write the appropriate MDC clock divisor to the gem.network_config[mdc_clock_division] bit.
+    // ?? QUAL CLOCK ESCOLHER?
+    Cadence::set_bits(NETWORK_CONFIG, MDC_CLOCK_DIVISION);
+
+
+    // 2. Set the MAC address.
+    // Write to the gem.spec_add1_bottom register.
+    
+
+    // Write to the gem.spec_add1_top register.
+    
+
+
+
+
     
 }
 
 void Cadence::set_reg(unsigned long int pointer, unsigned int value) {
-    Reg32 * p = reinterpret_cast<Reg32 *>(ETH_BASE + pointer);
+    Reg32 * p = reinterpret_cast<Reg32 *>(Memory_Map::ETH_BASE + pointer);
     Reg32 v = reinterpret_cast<Reg32>(value);
     *p = v;
     cout << v << endl;
 }
 
 void Cadence::set_bits(unsigned long int pointer, unsigned int value) {
-    Reg32 * p = reinterpret_cast<Reg32 *>(ETH_BASE + pointer);
+    Reg32 * p = reinterpret_cast<Reg32 *>(Memory_Map::ETH_BASE + pointer);
     Reg32 v = reinterpret_cast<Reg32>(value);
-    *p = *p | value;
+    *p = *p | v;
 }
 
 __END_SYS
