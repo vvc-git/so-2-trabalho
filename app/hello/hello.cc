60cddd7862b5434eb8b062427f2444eb520013b8
jdanprad0 <jdanprad0@gmail.com>
2023-09-16 15:56:12 -0300

    voltando hello ao normal

--- app/hello/hello.cc
+++ app/hello/hello.cc
@@ -1,25 +1,12 @@
 #include <utility/ostream.h>
 
-#include <machine/riscv/riscv_cadence.h>
-#include <utility/string.h>
-#include <machine/riscv/sifive_u/sifive_u_memory_map.h>
-
 using namespace EPOS;
 
-// OStream cout;
+OStream cout;
 
 int main()
 {
-    // cout << "Hello world!" << endl;
-    cout << "\n\n ************ TESTANDO CADENCE ************\n\n" << endl;
-
-    Cadence net = Cadence();
-    int * addr = reinterpret_cast<int *>(Memory_Map::ETH_BASE + 0x18);
-
-    cout << &net << endl;
-    cout << &addr << endl;
-
-    cout << "\n\n ******************* FIM *******************\n\n" << endl;
+    cout << "Hello world!" << endl;
 
     return 0;
 }
